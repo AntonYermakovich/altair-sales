ebc4a97382439767383c11873a8e389f2bc2f463
Anton Yermakovich <antonyermakovich@gmail.com>
2025-01-01 22:56:43 +0300

    init project

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+// Show modal
+const btnsDemo = document.querySelectorAll(".btn__demo");
+
+btnsDemo.forEach((btn) => btn.addEventListener("click", showModalDemo));
+
+function showModalDemo() {
+  const modalDemo = document.querySelector(".modal__demo");
+
+  modalDemo.classList.add("modal_show");
+  modalDemo.addEventListener("click", (e) => {
+    e.target.classList.contains("modal_show") &&
+      e.target.classList.remove("modal_show");
+  });
+}
+
+// Open menu
+document.querySelector(".header__icon").addEventListener("click", openMenu);
+
+function openMenu() {
+  const headerContainer = document.querySelector(".header__container");
+  const menu = document.querySelector(".menu");
+  const overlay = document.querySelector(".overlay");
+
+  if (this.classList.contains("header__icon_active")) {
+    this.classList.remove("header__icon_active");
+    headerContainer.classList.remove("header__container_active");
+    overlay.classList.remove("overlay_show");
+    menu.classList.remove("menu_show");
+  } else {
+    this.classList.add("header__icon_active");
+    headerContainer.classList.add("header__container_active");
+    overlay.classList.add("overlay_show");
+    menu.classList.add("menu_show");
+
+    overlay.addEventListener("click", () => {
+      this.classList.remove("header__icon_active");
+      headerContainer.classList.remove("header__container_active");
+      overlay.classList.remove("overlay_show");
+      menu.classList.remove("menu_show");
+    });
+  }
+}
+
+// Initial sliders
+// Trust
+new Splide(".trust__splide-1", {
+  perPage: 7,
+  rewind: true,
+  arrows: false,
+  pagination: false,
+  type: "loop",
+  drag: "free",
+  gap: "20px",
+  autoScroll: {
+    speed: 0.5,
+  },
+  breakpoints: {
+    540: {
+      perPage: 3,
+    },
+  },
+}).mount(window.splide.Extensions);
+
+new Splide(".trust__splide-2", {
+  perPage: 7,
+  rewind: true,
+  arrows: false,
+  pagination: false,
+  type: "loop",
+  drag: "free",
+  gap: "20px",
+  autoScroll: {
+    speed: -0.5,
+  },
+  breakpoints: {
+    540: {
+      perPage: 3,
+    },
+  },
+}).mount(window.splide.Extensions);
